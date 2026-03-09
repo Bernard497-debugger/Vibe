138061fd8888e4b986f82795e9cada30cb19fabe
Bernard497-debugger <botsile55@gmail.com>
2026-03-09 09:54:08 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -3767,4 +3767,4 @@ def api_admin_wipe_posts():
 
 # ══════════════════════════════════════════════════════════════════════════
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=app.config["PORT"], debug=True)
+    app.run(host="0.0.0.0", port=app.config["PORT"], debug=False)
